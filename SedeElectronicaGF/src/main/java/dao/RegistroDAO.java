7f0b5177fe20a22b85f108fbe9c0bd685786736d
Miguel <miguel.fernandez.villanueva@gregoriofer.com>
2026-01-22 13:49:15 +0100

    DAOS

---
+++ SedeElectronicaGF/src/main/java/dao/RegistroDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import org.hibernate.Session;
+
+
+import org.hibernate.Transaction;
+
+import entities.Registro;
+import logic.HibernateUtils;
+
+public class RegistroDAO {
+
+	public static void insert(Registro r) {
+		try(Session sesion = HibernateUtils.getSessionFactory().openSession()){
+			Transaction tr = sesion.beginTransaction();
+			sesion.persist(r);
+			tr.commit();
+		}
+	}
+}
