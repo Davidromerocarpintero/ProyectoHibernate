575b46597055eb5e44f6c0883897b1e84e38952c
Miguel <miguel.fernandez.villanueva@gregoriofer.com>
2026-01-26 13:09:36 +0100

    Cambios
    CAmbios y correcciones en los DAO, Registro.jsp e inicio del servlet

--- SedeElectronicaGF/src/main/java/dao/RegistroDAO.java
+++ SedeElectronicaGF/src/main/java/dao/RegistroDAO.java
@@ -1,25 +1,49 @@
 package dao;
 
 import org.hibernate.Session;
-
-
 import org.hibernate.Transaction;
-
 import entities.Registro;
 import logic.HibernateUtils;
 
 public class RegistroDAO {
 
-	public static void insert(Registro r) {
-		try(Session sesion = HibernateUtils.getSessionFactory().openSession()){
-			Transaction tr = sesion.beginTransaction();
+	public static void insert(Registro r) throws Exception {
+		Transaction tr = null;
+		try (Session sesion = HibernateUtils.getSessionFactory().openSession();) {
+			tr = sesion.beginTransaction();
 			sesion.persist(r);
 			tr.commit();
+		} catch (Exception ex) {
+			if (tr != null) {
+				tr.rollback();
+			}
+			throw new Exception("RegistroDAO: Error al realizar el insert del Registro en la base de datos.");
 		}
+
 	}
 
-	public Registro buscarPorNumero(String trim) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public static Registro selectId(Registro r) throws Exception {
+
+		Transaction tr = null;
+		Registro rgto=null;
+		try (Session sesion = HibernateUtils.getSessionFactory().openSession();) {
+			tr = sesion.beginTransaction();
+			//Get permite recuperar el registro de inmediato y recoge null si no existe 
+			rgto=sesion.get(Registro.class,r.getIdRegistro());
+			
+			
+			tr.commit();
+			
+		} catch (Exception ex) {
+			if (tr != null) {
+				tr.rollback();
+			}
+			ex.printStackTrace();
+		} finally {
+			return rgto;
+		}
+
+
 	}
 }
